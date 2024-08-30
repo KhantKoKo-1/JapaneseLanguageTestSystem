485440aa2aa6a470c1d2e3273ce8ec10ef02543a
SG-KhantKoKo <softguide.khantkoko@gmail.com>
2024-08-30 19:36:24 +0900

    change link

--- layout/user/nav.php
+++ layout/user/nav.php
@@ -13,7 +13,8 @@
             <div class="row gx-0 align-items-center" style="height: 45px;">
                 <div class="col-lg-8 text-center text-lg-start mb-lg-0">
                     <div class="d-flex flex-wrap">
-                        <a href="https://maps.app.goo.gl/1irxWKnmmYM4m8Rh7" target="_blank" class="text-light me-4"><i class="fa fa-map-marker text-primary me-2"></i>Find
+                        <a href="https://maps.app.goo.gl/1irxWKnmmYM4m8Rh7" target="_blank" class="text-light me-4"><i
+                                class="fa fa-map-marker text-primary me-2"></i>Find
                             A Location</a>
                         <a href="#" class="text-light me-4"><i
                                 class="fa fa-phone text-primary me-2"></i>+01234567890</a>
@@ -38,17 +39,18 @@
 
         <div class="container-fluid position-relative p-0 mt-3">
             <nav class="navbar navbar-expand-lg navbar-light bg-white px-4 px-lg-5 py-3 py-lg-0">
-                <a href="index.html" class="navbar-brand p-0">
-                    <h1 class="text-primary m-0"><i class="fas fa-star-of-life me-3"></i>Japanese Language Test System</h1>
-                    <!-- <img src="img/logo.png" alt="Logo"> -->
-                </a>
+                <!-- <a href="javascript:void(0)" class="navbar-brand p-0"> -->
+                <h1 class="text-primary m-0"><i class="fas fa-star-of-life me-3"></i>Japanese Language Test System
+                </h1>
+                <!-- </a> -->
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="fa fa-bars"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0">
-                        <a href="<?php $user_base_url ."home.php"?>" class="nav-item nav-link">Home</a> 
-                        <a href="<?php echo $user_base_url. "home.php#questions" ?>" class="nav-item nav-link">Questions</a>
+                        <a href="<?php $user_base_url ."home.php"?>" class="nav-item nav-link">Home</a>
+                        <a href="<?php echo $user_base_url. "home.php#questions" ?>"
+                            class="nav-item nav-link">Questions</a>
                         <a href="#footer" class="nav-item nav-link">Contact Us</a>
                         <a type="button" id="logout" class="nav-item nav-link" onClick>Log Out</a>
                         <!-- <a type="button" class="btn btn-info btn-sm nav-item nav-link">logOut</a> -->
